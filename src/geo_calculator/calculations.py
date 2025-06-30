a5fc2d87d1ceb3cca4d61fa118b01d9493638e33
Mattias A. Pedersen-Estrada <127201965+mattiasestrada@users.noreply.github.com>
2025-06-30 12:59:49 +0000

    Add docstrings

--- src/geo_calculator/calculations.py
+++ src/geo_calculator/calculations.py
@@ -1,7 +1,19 @@
 def find_average(array):
+    """Calculate the average of some numbers
+    Args:
+        numbers: A sequence of numbers
+    Returns:
+        The average value of the numbers
+    """
     return sum(array) / len(array)
 
 
 def gardners_equation(velocity):
+    """Calculate the bulk density of the lithology from seismic P-wave velocity using Gardners equation
+    Args:
+        velocity: Velocity of P-wave in m/s
+    Returns:
+        density: Bulk density of the lithology
+    """
     density = 0.31*(velocity**0.25)
     return density
